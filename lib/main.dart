9126e3839cd95474916238a034cae7f4f8db4d60
Hakimi Mdnoor <hakimimdnoor90@gmail.com>
2022-12-06 11:45:30 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:alumni_hub/firebase_options.dart';
 import 'package:alumni_hub/views/splash.screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
